e48247aa601516756535683e11e9e9a77ba69b76
JLP-DAM <2223_jan.lazaro@iticbcn.cat>
2025-11-25 19:58:54 +0100

    Treball classe 25/11/2025

--- app/src/main/java/com/notekeeper/MainActivity.kt
+++ app/src/main/java/com/notekeeper/MainActivity.kt
@@ -1,28 +1,40 @@
 package com.notekeeper
 
+import android.content.Context
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
+import android.util.TypedValue
+import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
+
+fun dpToPx(dp: Float, context: Context): Int {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp,
+        context.getResources().getDisplayMetrics()
+    ).toInt()
+}
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        var menuCardView = findViewById<CardView>(R.id.menu)
-        var menuImageButton = findViewById<ImageView>(R.id.menu_button)
+        val menuCardView = findViewById<CardView>(R.id.menu)
+        val menuImageButton = findViewById<ImageView>(R.id.menu_button)
 
         menuImageButton.setOnClickListener {
+            val displayMetrics = DisplayMetrics()
+
             Log.d("OKAY", "THIS WORKS");
-            menuCardView.animate().scaleX(300.0f)
-            menuCardView.width = 1
-            // 😭
+            Log.d("SIZE", "" + TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300f, resources.displayMetrics).toInt())
+            menuCardView.setLayoutParams(ViewGroup.LayoutParams(dpToPx(300f, applicationContext), applicationContext.resources.displayMetrics.heightPixels))
+
         }
     }
 }

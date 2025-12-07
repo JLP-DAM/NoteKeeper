846f846709a75587925897a0ba66e9fa1ee41413
Akasha Karam <akashakaram3@gmail.com>
2025-12-07 20:18:21 +0100

    En process

--- app/src/main/java/com/notekeeper/nota_compartir.kt
+++ app/src/main/java/com/notekeeper/nota_compartir.kt
@@ -1,10 +1,13 @@
 package com.notekeeper
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.ImageButton
 
 class nota_compartir : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,32 @@ class nota_compartir : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnSave = findViewById<ImageButton>(R.id.btn_save)
+        btnSave.setOnClickListener {
+            guardarNotaCompartir()
+
+            val intent = Intent(this, compartir::class.java)
+            startActivity(intent)
+
+        }
+
+        val btnClose = findViewById<ImageButton>(R.id.btn_close)
+        btnClose.setOnClickListener {
+            finish()
+        }
+
+        val saveButton = findViewById<Button>(R.id.save_button)
+        saveButton.setOnClickListener {
+            guardarNotaCompartir()
+
+            val intent = Intent(this, compartir::class.java)
+            startActivity(intent)
+
+        }
+    }
+
+    private fun guardarNotaCompartir() {
+
     }
 }

0dd2fab4035bf801bf24b460d7a9701d153e96ca
Akasha Karam <akashakaram3@gmail.com>
2025-12-07 16:36:34 +0100

    En process

--- app/src/main/java/com/notekeeper/agenda.kt
+++ app/src/main/java/com/notekeeper/agenda.kt
@@ -1,6 +1,8 @@
 package com.notekeeper
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.CalendarView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,13 @@ class agenda : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_agenda)
+
+        val calendarView: CalendarView = findViewById(R.id.calendarView)
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val intent = Intent(this, nota_agenda::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/notekeeper/compartir.kt
+++ app/src/main/java/com/notekeeper/compartir.kt
@@ -1,6 +1,9 @@
 package com.notekeeper
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,19 @@ class compartir : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_compartir)
+
+        val btnLogin: Button = findViewById(R.id.btn_login)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, iniciar_sesion::class.java)
+            startActivity(intent)
+        }
+
+        val text_sign_in: TextView = findViewById(R.id.text_sign_in)
+        text_sign_in.setOnClickListener {
+            val intent = Intent(this, registro::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

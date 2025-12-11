70dc8ba7f32d7e88f5f5bbebf5034b7393dca7ee
Akasha Karam <akashakaram3@gmail.com>
2025-12-11 16:34:30 +0100

    Areglat les pantalles

--- app/src/main/java/com/notekeeper/ChangePassword.kt
+++ app/src/main/java/com/notekeeper/ChangePassword.kt
@@ -1,35 +1,20 @@
 package com.notekeeper
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
-import android.widget.ImageButton
 
-class cerrar_sesion : AppCompatActivity() {
+class ChangePassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cerrar_sesion)
+        setContentView(R.layout.activity_change_password)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnLogin = findViewById<Button>(R.id.btn_login)
-
-        btnLogin.setOnClickListener {
-            val intent = Intent(this, iniciar_sesion::class.java)
-            startActivity(intent)
-        }
-
-        val btnMenu = findViewById<ImageButton>(R.id.btn_menu)
-        btnMenu.setOnClickListener {
-            finish()
         }
     }
 }
--- app/src/main/java/com/notekeeper/LogOut.kt
+++ app/src/main/java/com/notekeeper/LogOut.kt
@@ -1,29 +1,20 @@
 package com.notekeeper
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class registro : AppCompatActivity() {
+class LogOut : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_registro)
+        setContentView(R.layout.activity_log_out)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnLogin = findViewById<Button>(R.id.loginButton)
-
-        btnLogin.setOnClickListener {
-            val intent = Intent(this, NoteShare::class.java)
-            startActivity(intent)
         }
     }
 }

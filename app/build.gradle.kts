3c0b330d4fc3d89f633d0dffa7f0aa001a59f574
JLP-DAM <2223_jan.lazaro@iticbcn.cat>
2025-12-15 18:59:13 +0100

    Treball classe 15/12/2025

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.core:core-splashscreen:1.0.0")
+    implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.29")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/notekeeper/MainActivity.kt
+++ app/src/main/java/com/notekeeper/MainActivity.kt
@@ -1,27 +1,21 @@
 package com.notekeeper
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.util.TypedValue
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.fragment.app.Fragment
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-fun dpToPx(dp: Float, context: Context): Int {
-    return TypedValue.applyDimension(
-        TypedValue.COMPLEX_UNIT_DIP,
-        dp,
-        context.resources.displayMetrics
-    ).toInt()
-}
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.main)
+        val splashScreen = installSplashScreen()
 
-        BottomNavigationMenu().createMenu(findViewById(R.id.bottomNavigationView))
+
+
+        splashScreen.setKeepOnScreenCondition {
+            true
+        }
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+
     }
 }
